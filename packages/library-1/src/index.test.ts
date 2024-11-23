50c343cabf2b6ddf786b96f6d4b8c58640b2d432
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2024-11-23 14:52:07 -0500

    init

---
+++ packages/library-1/src/index.test.ts
@@ -0,0 +1,5 @@
+import { expect, test } from 'vitest';
+
+test('does something cool', () => {
+	expect(true).toBe(true);
+});
---
+++ packages/library-2/src/index.ts
@@ -0,0 +1,5 @@
+import { bar } from 'library-1';
+
+export const foo = 'foo';
+
+console.log(foo, bar);
